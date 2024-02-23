a046904d78df65b7f0ef4b89d54d3f32b5987b8a
jermspeaks <jeremy.wong@clearlabs.com>
2024-02-23 12:08:27 -0800

    Chapter 1 through 4

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -84,5 +84,7 @@ export type InvoiceForm = {
   id: string;
   customer_id: string;
   amount: number;
+  // In TypeScript, this is called a string union type.
+  // It means that the "status" property can only be one of the two strings: 'pending' or 'paid'.
   status: 'pending' | 'paid';
 };
